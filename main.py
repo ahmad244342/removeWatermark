4fc63e6725b69272d74065055ba0a212b125ebf2
ahmad <59347239+ahmad244342@users.noreply.github.com>
2020-07-01 20:10:00 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import cv2
 
-img = cv2.imread("examples/safrudin.png")
+img = cv2.imread("safrudin.png")
 img = cv2.resize(img, (600, 350))
-mask = cv2.imread("examples/safrudinMask.png")
+mask = cv2.imread("safrudinMask.png")
 mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
 mask = cv2.resize(mask, (600, 350))
 output = cv2.inpaint(img, mask, 3, flags=cv2.INPAINT_NS)
