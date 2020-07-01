a1c2bd035dcc3ae2795a62a27bad47535fe7a442
ahmad <59347239+ahmad244342@users.noreply.github.com>
2020-07-01 20:09:00 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import cv2
+
+img = cv2.imread("examples/safrudin.png")
+img = cv2.resize(img, (600, 350))
+mask = cv2.imread("examples/safrudinMask.png")
+mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+mask = cv2.resize(mask, (600, 350))
+output = cv2.inpaint(img, mask, 3, flags=cv2.INPAINT_NS)
+
+cv2.imshow("original", img)
+cv2.imshow("mask", mask)
+cv2.imshow("output", output)
+
+cv2.waitKey(0)
